90b25383049df5653539065b46e2a412e7d739f9
amichel23 <lexmichel2014@gmail.com>
2022-05-12 22:11:09 -0400

    completed assignment

--- news-site/src/components/footer.js
+++ news-site/src/components/footer.js
@@ -1,7 +1,15 @@
 import react from "react";
+import { useState } from "react";
 import image from './images/logo_light.png'
 
 const Footer = () => {
+
+    const [emailText, setEmailText] = useState("");
+
+    const handleClick = () => {
+        alert('your email has been saved');
+    }
+
     return (
         <div>
             <footer>
@@ -12,7 +20,14 @@ const Footer = () => {
         <div>
             <h3>Sign-up for our Newsletter</h3>
             <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ea impedit libero, beatae animi provident nesciunt molestias ipsam nemo ad. </p>
-            {/* <input type="email" name="" placeholder="enter your email" id=""><span><button class="footer-button" >Subscribe</button></span> */}
+            <input 
+            type="emailText" 
+            name="emailText" 
+            placeholder="enter your email"
+            value={emailText}
+            onChange={e => setEmailText(e.target.value) }
+            id=""/><span>
+            <button onClick={() => this.handleClick()} className="footer-button" >Subscribe</button></span>
         </div>
         <div className="footer-items">
             <h3>Connect with Us!</h3>
